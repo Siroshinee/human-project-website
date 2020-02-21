5ca4108594992e6ac5bd2193490ba75ecb32c3c9
Siroshinee <siroshinee04@gmail.com>
2020-02-21 12:10:04 +0530

    feature updated
    time

--- client/js/feature.js
+++ client/js/feature.js
@@ -2,11 +2,17 @@ var featureSubmitBtn = document.getElementById("featureSubmitBtn");
 var featureFrm = document.getElementById("featureFrm");
 var featureLoadingDiv = document.getElementById("featureLoadingDiv");
 
-featureSubmitBtn.addEventListener("click", function() {
+featureSubmitBtn.addEventListener("click", async function() {
   // Make the loader div visible
   featureLoadingDiv.classList.remove("human-removed");
   // Hide the form from the user
   featureFrm.classList.add("human-removed");
+  await onfeaturesubmit();
+  featureFrm.reset();
+  featureLoadingDiv.classList.add("human-removed");
+  featureFrm.classList.remove("human-removed");
+  await onfeatureload();
+
 });
 
 async function onfeatureload() {
@@ -14,20 +20,39 @@ async function onfeatureload() {
   var result = await response.json();
 
   var feature1 = result[0]; // accesses tbe third element in the array
-  var feature2 = result[1]
-  var feature3 = result[2]
+  var feature2 = result[1];
+  var feature3 = result[2];
 
 /*document.getElementById("feature1").innerHTML =
     feature1.body + "<span class='badge badge-success'>" + feature1.author + ", " + feature1.time + "</span>";
 */
 
 var featurelist = document.getElementById("feature-list");
+
+featurelist.innerHTML = "";
+
 for( var i=0; i < result.length; i++) {
   var lielement = document.createElement("li");
   lielement.innerHTML = result[i].body + "<span class='badge badge-success'>" + result[i].author + ", " + result[i].time + "</span>";
   lielement.className = "list-group-item";
   featurelist.appendChild(lielement);
+  }
 }
+
+async function onfeaturesubmit(){
+  var response = await fetch("http://localhost:3000/features" ,
+  {
+    method : "post",
+    headers : {
+      "content-type" : "application/json"
+    },
+    body : JSON.stringify({
+      name : document.getElementById("suggested-name").value ,
+      feature : document.getElementById("suggested-feature").value
+    })
+  });
 }
+ 
+
 onfeatureload();
 
--- server/index.js
+++ server/index.js
@@ -1,8 +1,28 @@
 const express = require("express"); // import
 const cors = require("cors"); // import cors
+var timeAgo = require('node-time-ago');
 const app = express(); // creating an express app
 
+
+app.use(express.json()); //needed when working with POST requests (body contains JSON data)
 app.use(cors()); // enabling cors to allow requests to come inside the server
+
+var features = [
+  {
+    body: "Feature 1",
+    author: "Shiro",
+    time: Date.now()
+  },
+  {
+    body: "Feature 2",
+    author: "Student2",
+    time: Date.now()
+  },
+  {
+    body: "Feature 3",
+    author: "Student3",
+    time: Date.now()
+  }];
 
 app.get("/", function(req, res) {
   res.send("Hello World");
@@ -13,25 +33,24 @@ app.get("/dogs", function(req, res) {
 });
 
 app.get("/features", function(req, res) {
-  var features = [
-    {
-      body: "Feature 1",
-      author: "Shiro",
-      time: Date.now()
-    },
-    {
-      body: "Feature 2",
-      author: "Student2",
-      time: Date.now()
-    },
-    {
-      body: "Feature 3",
-      author: "Student3",
-      time: Date.now()
-    }
-  ];
+  features.every(f=> (f.time = timeAgo(f.time)));
   res.send(features);
 });
+
+app.post("/features", function(req,res){
+  console.log("This method was called");
+  var newFeatureToBeAddedToArray = {
+    body: req.body.feature,
+    author:req.body.name,
+    time:Date.now()
+  };
+
+  features.push(newFeatureToBeAddedToArray);
+  console.log(features);
+  res.send("Hello world");
+});
+
+
 
 app.listen(3000, function() {
   console.log("Application is running on Port 3000");
